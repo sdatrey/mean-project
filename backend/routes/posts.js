e130695ff3f0e280702913e64ff841535746f396
sdatrey <shouryadevatrey@gmail.com>
2020-12-15 22:12:55 +0530

    Upload files using backend

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,8 +1,30 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
 
 const Post = require('../models/post');
 
+const MIME_TYPE_MAP = {
+    'image/png': 'png',
+    'image/jpeg': 'jpg',
+    'image/jpg': 'jpg'
+};
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        const isValid = MIME_TYPE_MAP[file.mimetype];
+        let error = new Error("Invalid mime type");
+        if(isValid){
+            error = null;
+        }
+        cb(null, "backend/images");
+    },
+    filename: (req, file, cb) => {
+        const name = file.originalname.toLowerCase().split(' ').join('-');
+        const ext = MIME_TYPE_MAP[file.mimetype];
+        cb(null, name + '-' + Date.now() + '.' + ext);
+    }
+});
 router.get('',(req, res, next) =>{
     Post.find().then(documents => {
         res.status(200).json({
@@ -12,7 +34,7 @@ router.get('',(req, res, next) =>{
     })
 });
 
-router.post('', (req, res, next) => {
+router.post('', multer(storage).single("image"), (req, res, next) => {
     const post = new Post({
         title: req.body.title,
         content: req.body.content
